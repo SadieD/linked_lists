1e5492933a397de7e9a7a8cf6775229f5d04c3be
sadied <sadie@cnoss.us>
2017-05-29 10:34:00 -0700

    Changed default value of node.

--- linked_lists.rb
+++ linked_lists.rb
@@ -81,7 +81,7 @@ end
 class Node
   attr_accessor :value, :next_node
   
-  def initialize(value, next_node = nil)
+  def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
   end
